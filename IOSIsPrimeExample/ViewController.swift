7f7662e04f6bdcc0be3b2207a9e8b6e9a815d77c
Petho Alpar <pethoalpar@192-168-0-101.rdsnet.ro>
2017-02-19 14:30:23 +0200

    First commit

--- IOSIsPrimeExample/ViewController.swift
+++ IOSIsPrimeExample/ViewController.swift
@@ -9,7 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var resultField: UILabel!
+   
+    @IBOutlet weak var numberField: UITextField!
+    
+    func isPrime(number : Int) -> Bool {
+        if number < 2 {
+            return false
+        } else if number == 2 {
+            return true
+        }
+        var i = 2
+        let limit = Int(ceil(sqrt(Double(number))))
+        while i <= limit{
+            if number % i == 0{
+                return false
+            }
+            i += 1
+        }
+        return true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +40,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func check(_ sender: Any) {
+        let number = Int(numberField.text!)
+        if number == nil {
+            resultField.text = "Is not a valid number"
+        }else{
+            let result = isPrime(number: number!)
+            resultField.text = result ? "Is prime" : "Is not prime"
+        }
+        
+    }
 }
 
